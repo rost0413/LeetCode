4e89b98020739fc9fcf3079213fe2849e7d96368
zwx <ideazwx@gmail.com>
2012-06-28 21:34:06 -0400

    gray code

--- Gray_Code.cpp
+++ Gray_Code.cpp
@@ -1,34 +1,46 @@
 /*
-Author:     Weixian Zhou, ideazwx@gmail.com
-Date:       June 28, 2012
-Problem:    Gray Code
-Difficulty: easyt
-Source:     http://www.leetcode.com/onlinejudge
-Notes:
-The gray code is a binary numeral system where two successive values differ in
-only one bit.
+ Author:     Weixian Zhou, ideazwx@gmail.com
+ Date:       June 28, 2012
+ Problem:    Gray Code
+ Difficulty: hard
+ Source:     http://www.leetcode.com/onlinejudge
+ Notes:
+ The gray code is a binary numeral system where two successive values differ in
+ only one bit.
 
-Given a non-negative integer n representing the total number of bits in the code,
-print the sequence of gray code. A gray code sequence must begin with 0.
+ Given a non-negative integer n representing the total number of bits in the code,
+ print the sequence of gray code. A gray code sequence must begin with 0.
 
-For example, given n = 2, return [0,1,3,2]. Its gray code sequence is:
+ For example, given n = 2, return [0,1,3,2]. Its gray code sequence is:
 
-00 - 0
-01 - 1
-11 - 3
-10 - 2
+ 00 - 0
+ 01 - 1
+ 11 - 3
+ 10 - 2
 
-Solution:
-n = 3
-000 - 0
-001 - 1
-011 - 3
-010 - 2
-110 - 6
-111 - 7
-101 - 5
-100 - 4
-*/
+ Solution:
+ n = 3
+ 000 - 0
+ 001 - 1
+ 011 - 3
+ 010 - 2
+ 110 - 6
+ 111 - 7
+ 101 - 5
+ 100 - 4
+
+ If we try to find the regular of the gray code, we will find that the first
+ digit (from right to left) has certain pattern '0110', repeat every four
+ string. And the second digit has pattern '00111100', repeat every eight string.
+ The third digit has pattern '0000111111110000'...
+
+ After wiki, gray code is very famous math notion. And the generation function
+ actually only takes 1 lines of code:
+ 	unsigned int binaryToGray(unsigned int num)
+	{
+        return (num >> 1) ^ num;
+	}
+ */
 #include <vector>
 #include <set>
 #include <climits>
@@ -41,26 +53,37 @@ using namespace std;
 
 class Solution {
 public:
-    vector<int> result;
+	vector<int> result;
 
-    void recursion(int start, int num, int n) {
-        if (start == n) {
-            result.push_back(num);
-        }
-        for (int i = start; i < n; i++) {
-            for (int j = 0; j < 2; j++) {
-                recursion(i + 1, num ^ (j << (n - i - 1)), n);
-            }
-        }
-    }
+	/*
+	 * n is the string sequence number, it determines which code to
+	 * use (0 or 1); m is the digit sequence number, it determines
+	 * which pattern to use ('0110', '00111100', ...)
+	 */
+	void genDigit(int& code, int n, int m) {
+		int len = 1 << (m + 2);
+		n &= ((1 << (m + 2)) - 1);
+		if (n >= (len >> 2) && n < len - (len >> 2)) {
+			code |= 1 << m;
+		}
+		else {
+			code &= ~(1 << m);
+		}
+	}
 
-    vector<int> grayCode(int n) {
-        result.clear();
-        recursion(0, 0, n);
+	vector<int> grayCode(int n) {
+		int round = 1 << n;
+		int code;
 
-        return result;
-    }
+		result.clear();
+		for (int i = 0; i < round; i++) {
+			code = 0;
+			for (int j = 0; j < n; j++) {
+				genDigit(code, i, j);
+			}
+			result.push_back(code);
+		}
+		return result;
+	}
 };
-
-
 
